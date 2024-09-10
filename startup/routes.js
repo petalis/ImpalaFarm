92fed865fd4ea3374df582956cbbfad4f04dd907
petalis-787 <koripeter787@gmail.com>
2024-09-10 17:54:50 +0300

    build command

--- startup/routes.js
+++ startup/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+//const cors = require('cors');
 const cookieSession = require('cookie-session');
 const error = require('../middlewares/error');
 const adminLoginMiddleware = require('../middlewares/adminLoginMiddleware');
@@ -29,8 +30,10 @@ const inventory = require('../routes/pos/inventory');
 
 
 module.exports = function (app) {
+    //app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
+
     app.use(cookieSession({
         name: 'session',
         keys:['cookieSessionKey']
